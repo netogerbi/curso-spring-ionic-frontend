c4466680c37b3ab96e01cb5051fb5019edbbfd55
Neto Gerbi <netogerbi2@hotmail.com>
2018-12-29 15:41:59 -0200

    Tratamento padrão para outros erros com o Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS
 import { Observable } from "rxjs/Rx";
 import { Injectable } from "@angular/core";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class  ErrorInterceptor implements HttpInterceptor{
 
-  constructor(public storage: StorageService){
+  constructor(public storage: StorageService, public alertCtrl: AlertController){
   }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -21,15 +22,47 @@ export class  ErrorInterceptor implements HttpInterceptor{
             }
 
             switch (errorObj.status){
+              case 401:
+                this.handle401();
               case 403:
                 this.handle403();
                 break;
+              default:
+                this.handleDefaultError(errorObj);
             }
 
             console.log("Erro detectado pelo interceptor:\n",errorObj);
             return Observable.throw(errorObj);
 
         }) as any;
+    }
+
+    handleDefaultError(errorObj){
+      let alert = this.alertCtrl.create({
+        title: "Erro "+errorObj.status+": "+errorObj.error,
+        message: errorObj.message,
+        enableBackdropDismiss: false,
+        buttons: [
+          {
+            text: "OK"
+          }
+        ]
+      });
+      alert.present();
+    }
+
+    handle401(): any {
+      let alert = this.alertCtrl.create({
+        title: "Erro 401: Falha de Autenticação",
+        message: "Email ou senha incorretos!",
+        enableBackdropDismiss: false,
+        buttons: [
+          {
+            text: "OK"
+          },
+        ]
+      });
+      alert.present();
     }
 
     //caso possivel localuser invalido no storage, remove.
