ac3de50a992a434d534c131e073f1f69350e2baf
Neto Gerbi <netogerbi2@hotmail.com>
2019-01-09 20:21:18 -0200

    Terminando as funcionalidades do carrinho

--- src/services/domain/cart-service.ts
+++ src/services/domain/cart-service.ts
@@ -16,7 +16,7 @@ export class CartService{
   }
 
   getCart(): Cart {
-    let cart = this.storage.getCart();
+    let cart: Cart = this.storage.getCart();
     if(cart == null){
       cart = this.createOrClearCart();
     }
@@ -31,5 +31,47 @@ export class CartService{
     }
     this.storage.setCart(cart);
     return cart;
+  }
+
+  removeProduto(produto: ProdutoDTO): Cart {
+    let cart = this.getCart();
+    let position = cart.items.findIndex(x => x.produto.id == produto.id);
+    if(position != -1){
+      cart.items.splice(position,1);
+    }
+    this.storage.setCart(cart);
+    return cart;
+  }
+
+  ioncreaseQuantity(produto: ProdutoDTO): Cart {
+    let cart = this.getCart();
+    let position = cart.items.findIndex(x => x.produto.id == produto.id);
+    if(position != -1){
+      cart.items[position].quantidade++;
+    }
+    this.storage.setCart(cart);
+    return cart;
+  }
+
+  decreaseQuantity(produto: ProdutoDTO): Cart {
+    let cart = this.getCart();
+    let position = cart.items.findIndex(x => x.produto.id == produto.id);
+    if(position != -1){
+      cart.items[position].quantidade--;
+      if(cart.items[position].quantidade < 1){
+        cart = this.removeProduto(produto);
+      }
+    }
+    this.storage.setCart(cart);
+    return cart;
+  }
+
+  total(): number {
+    let cart = this.getCart();
+    let sum = 0;
+    for(let i=0 ; i < cart.items.length; i++){
+      sum = sum + cart.items[i].produto.preco * cart.items[i].quantidade;
+    }
+    return sum;
   }
 }
