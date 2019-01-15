fa01859306ee5e7af0c0daec93b114a4f17be431
Neto Gerbi <netogerbi2@hotmail.com>
2019-01-15 19:23:15 -0200

    Armazenamento dos dados do pedido

--- src/pages/pick-adress/pick-adress.ts
+++ src/pages/pick-adress/pick-adress.ts
@@ -3,6 +3,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { StorageService } from '../../services/storage.service';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { HomePage } from '../home/home';
+import { CartService } from '../../services/domain/cart-service';
 
 @IonicPage()
 @Component({
@@ -12,12 +15,14 @@ import { ClienteService } from '../../services/domain/cliente.service';
 export class PickAdressPage {
 
   items: EnderecoDTO[];
+  pedido: PedidoDTO;//trafega ao longo das paginas preenchendo o pedido
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService
+    public clienteService: ClienteService,
+    public cartService: CartService,
     ) {}
 
   ionViewDidLoad() {
@@ -26,9 +31,31 @@ export class PickAdressPage {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response =>{
           this.items = response["enderecos"];
+
+          //primeira instanciação do PedidoDTO
+          let cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: {id: response["id"]},
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens: cart.items.map(x => { return {quantidade: x.quantidade, produto: {id: x.produto.id}} })
+          }
         },
-        error => {});
+        error => {
+          if(error.status == 403){
+            this.navCtrl.setRoot("HomePage");
+          }
+        });
+    }else{
+      this.navCtrl.setRoot("HomePage");
     }
   }
+
+  nextPage(item: EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido);
+  }
+
 
 }
