b073daa25e1a0b89a41a82cd3a7ce9a3e519e967
Neto Gerbi <netogerbi2@hotmail.com>
2019-01-16 21:30:43 -0200

    Informando pedido registrado ao cliente

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(
     public navCtrl: NavController,
@@ -58,7 +59,7 @@ export class OrderConfirmationPage {
     this.pedidoService.insert(this.pedido)
       .subscribe(response =>{
         this.cartService.createOrClearCart();
-        console.log(response.headers.get("location"));
+        this.codPedido = this.extractId(response.headers.get("location"));
       },
       error => {
         if(error.status == 403){
@@ -70,5 +71,16 @@ export class OrderConfirmationPage {
   back(){
     this.navCtrl.setRoot("CartPage");
   }
+
+  home(){
+    this.navCtrl.setRoot("CategoriasPage");
+  }
+
+  private extractId(location: string): string {
+    let position = location.lastIndexOf("/");
+    return location.substring(position + 1, location.length);
+  }
+
+
 
 }
