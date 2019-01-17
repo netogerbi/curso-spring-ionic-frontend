8df94edc4ff194d28564e294762fd8253f5f2d62
Neto Gerbi <netogerbi2@hotmail.com>
2019-01-17 21:49:00 -0200

    Usando cordova para tirar foto com a camera - 404 not found aparecendo no console do browser

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,6 +4,7 @@ import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -13,39 +14,60 @@ import { API_CONFIG } from '../../config/api.config';
 export class ProfilePage {
 
   cliente: ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    private camera: Camera) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
-    if(localUser && localUser.email){
+    if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
-        .subscribe(response=>{
+        .subscribe(response => {
           this.cliente = response as ClienteDTO; //apesar do objeto vir com campos a mais, este casting informa que haverá os campos do DTO
           //procura imagem no bucket
           this.getImageIfExists();
         },
-        error=>{
-          if(error.status == 403){
-            this.navCtrl.setRoot("HomePage");
-          }
-        });
-    }else{
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot("HomePage");
+            }
+          });
+    } else {
       this.navCtrl.setRoot("HomePage");
     }
   }
 
-  getImageIfExists(){
+  getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
-      .subscribe(response=>{
+      .subscribe(response => {
         this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
       },
-      error=>{});
+      error => { });
+  }
+
+  getCameraPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+    });
   }
 
 }
