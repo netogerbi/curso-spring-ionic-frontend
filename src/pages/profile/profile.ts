a7d3c2b1d7948c60d88acf61162fd55801506c96
Neto Gerbi <netogerbi2@hotmail.com>
2019-02-17 16:55:17 -0300

    Pegando imagem da galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -73,6 +73,25 @@ export class ProfilePage {
     });
   }
 
+  getGalleryPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+    });
+  }
+
   sendPicture() {
     this.clienteService.uploadPictrue(this.picture)
       .subscribe(response => {
